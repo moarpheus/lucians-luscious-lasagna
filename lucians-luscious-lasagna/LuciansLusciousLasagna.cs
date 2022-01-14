9696e32e004cfe5affae6bf0c0512dd4a7f57bac
JG <moarpheus@gmail.com>
2022-01-14 09:11:02 +0000

    all conditions matched

--- lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -7,8 +7,20 @@ class Lasagna
     }
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minutes)
+    {
+        return ExpectedMinutesInOven() - minutes;
+    }
 
     // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers)
+    {
+        return layers * 2;
+    }
 
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int minutes)
+    {
+        return minutes + PreparationTimeInMinutes(layers);
+    }
 }
